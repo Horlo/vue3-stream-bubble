9295a7eaeb32f6b1d4e321328b8e4d7e8c904fff
樊玥 <478292550@QQ.COM>
2026-03-21 00:54:00 +0800

    feat: add library entry point

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// src/index.ts
+import StreamBubble from './components/StreamBubble/index.vue'
+
+export { StreamBubble }
+export default StreamBubble
+
+// 类型导出
+export type { StreamBubbleProps, MarkdownToken, TokenWithKey } from './components/StreamBubble/types'
